c3093794770614f518c49086718db11607cc5391
rayyada <rayada@ucsd.edu>
2019-08-05 18:08:16 -0700

    Updates

---
+++ precache-manifest.a585868415c7bdcd3e4d2bdf8aa8cfec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8ba8edfaf9220e21ce96ddc65b812c",
+    "url": "/React-Tutorial/index.html"
+  },
+  {
+    "revision": "921af123528a5c7e72f4",
+    "url": "/React-Tutorial/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React-Tutorial/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "921af123528a5c7e72f4",
+    "url": "/React-Tutorial/static/js/main.c9389cc2.chunk.js"
+  },
+  {
+    "revision": "e2f549e02207fab0f75f",
+    "url": "/React-Tutorial/static/js/runtime~main.02bebb52.js"
+  }
+]);
